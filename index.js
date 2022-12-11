86c2b1d505b9080ceba00ebb7541046efb6d1a03
zaidsheriff786 <mszaid.007@gmail.com>
2022-12-11 16:19:41 +0530

    config changes

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const express = require("express");
+const bodyParser = require("body-parser");
 const cors = require("cors");
 const fileUpload = require("express-fileupload");
 const fs = require("fs");
@@ -11,23 +12,29 @@ const app = express();
 
 app.use(cors());
 app.use(fileUpload());
-app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ limit: "50mb" }));
-app.use(express.static(path.resolve(__dirname, "../public/images/")));
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(
+  bodyParser.urlencoded({
+    limit: "50mb",
+    extended: true,
+  })
+);
+
+app.use(express.static(path.resolve(__dirname, "./public/images/")));
 // Have Node serve the files for our built React app
-app.use(express.static(path.resolve(__dirname, "../build")));
+app.use(express.static(path.resolve(__dirname, "./build")));
 
 app.get("/api", (req, res) => {
   res.json({ message: "Hello from server!" });
 });
 
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../build", "index.html"));
+  res.sendFile(path.resolve(__dirname, "./build", "index.html"));
 });
 
 const capturedImage = async (req, res, next) => {
   try {
-    const targetPath = path.join(__dirname, "../public/images/ocr_image.jpeg");
+    const targetPath = path.join(__dirname, "./public/images/ocr_image.jpeg");
     let imgdata = req.body.img; // get img as base64
     const base64Data = imgdata.replace(/^data:([A-Za-z-+/]+);base64,/, ""); // convert base64
     fs.writeFileSync(targetPath, base64Data, { encoding: "base64" }); // write img file
@@ -51,13 +58,13 @@ app.post("/upload", (req, res) => {
   if (req.files) {
     let file = req.files.file;
     let fileName = file.name;
-    const location = path.join(__dirname, "../public/images/");
+    const location = path.join(__dirname, "./public/images/");
     file.mv(location + fileName, (err) => {
       if (err) {
         console.log(err);
         res.send(err);
       } else {
-        const location = path.join(__dirname, "../public/images/");
+        const location = path.join(__dirname, "./public/images/");
         const contents = fs.readFileSync(`${location}/${fileName}`, {
           encoding: "base64",
         });
